8a614cbd0150daafced8060a637b0fdb0b46f4c6
hexlo <hexlo@mindlab.dev>
2024-03-16 20:10:44 -0400

    Fixed version-getting script for changed website output

--- .scripts/get-terraria-version.sh
+++ .scripts/get-terraria-version.sh
@@ -3,4 +3,5 @@ curl -v -L --silent \
   -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36" \
   https://terraria.wiki.gg/wiki/Server#Downloads 2>&1 \
   | grep -o 'https://terraria.org/api/download/pc-dedicated-server/[^"]*' \
-  | sed 's#.*/terraria-server-##' | sed 's/\.zip//' | tail -1
+  | sed 's#.*/terraria-server-##' | sed 's/\.zip//' \
+  | head -n 1
